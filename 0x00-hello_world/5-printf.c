6978031813ae0bd3dbf9855d0b125bd18558d29e
Lich Gatkoi Puok <maliethkwoth@gmail.com>
2023-03-16 12:43:28 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,＼");
+	printf("with proper grammar, but the outcome is a piece of art,
+");
 	return (0);
 }
 
