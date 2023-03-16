04f6fc7f10707d38c72283c603ab850afa714233
Lich Gatkoi Puok <maliethkwoth@gmail.com>
2023-03-16 11:51:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#!/bin/bash
-#include<stdio.h>
+#include <stdio.h>
 /**
- *main-Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("＼"Programming is like building a multilingual puzzle");
+	puts("＼"Programming is like building a multilingual puzzle");
 	return(0);
 }
